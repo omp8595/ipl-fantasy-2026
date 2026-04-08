6e21595dba20d138c0bb8074d52f84b7cef8dd91
Om Prakash <omp8595@gmail.com>
2026-04-09 02:49:15 +0530

    add callFn to firebase lib

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getFunctions, httpsCallable } from 'firebase/functions'
 
 const firebaseConfig = {
   apiKey:            process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,10 +12,12 @@ const firebaseConfig = {
   appId:             process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
 
-// Prevent re-initialization in Next.js hot reload
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
 
 export const auth = getAuth(app)
-export const db   = getFirestore(app)
+export const db = getFirestore(app)
+export const functions = getFunctions(app)
+export const callFn = {
+  submitTeam: (data) => httpsCallable(functions, 'submitTeam')(data)
+}
 export default app
-
