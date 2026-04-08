56b844a7945518df870e3aff58456d9ee65ca579
Om Prakash <omp8595@gmail.com>
2026-04-09 02:13:09 +0530

    remove deprecated swcMinify from next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Allow firebase-admin in API routes
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
-  },
-}
-
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */ 
+const nextConfig = { 
+  reactStrictMode: true, 
+  experimental: { serverComponentsExternalPackages: ['firebase-admin'] }, 
+} 
+module.exports = nextConfig 
